fe97a25d38b7dea9884cc338b1e799656cc61d5f
Igor Makarevich <igor.makarevich@novometgroup.com>
2021-08-26 14:51:57 +0500

    First commit

---
+++ cmake-build-debug/CMakeFiles/Homework_16_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework_16_1.dir/main.cpp.obj"
+  "Homework_16_1.exe"
+  "Homework_16_1.exe.manifest"
+  "Homework_16_1.lib"
+  "Homework_16_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework_16_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+bool compare (float speed) {
+    return (speed >= -0.01) && (speed <= 0.01);
+}
+
+float speed_result (float speed, float delta) {
+    speed += delta;
+    if (speed > 150.0f) {
+        speed = 150.0f;
+    }
+    if (speed < 0.0f) {
+        speed = 0.0f;
+    }
+    return speed;
+}
+
+int main() {
+    float speed = .0f;
+    float delta = .0f;
+    std::cout << "Input the speed increase: ";
+    std::cin >> delta;
+    speed = speed_result(speed, delta);
+    while (!compare(speed)) {
+        std::cout << "Current speed: " << speed << std::endl;
+        std::cout << "Input the speed increase: ";
+        std::cin >> delta;
+        speed = speed_result(speed, delta);
+    }
+    std::cout << "You have stopped!";
+    return 0;
+}
